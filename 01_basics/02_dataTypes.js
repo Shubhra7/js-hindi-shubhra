a17b51a5ad9ef8687bdb0da9b2cd43f9bc236551
Shubhra7 <112610308+Shubhra7@users.noreply.github.com>
2024-03-29 14:40:00 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -6,3 +6,22 @@ console.log(3 + 3); // code readability should high
 
 // official site: tc39.es
 // or can use MDN
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string=""
+// boolean => true/false
+// null =>standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);

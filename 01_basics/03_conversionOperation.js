48f2e537c9c385d83645a277cbc0575d46526b04
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-03-30 16:00:57 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,9 +19,49 @@
 // "" =>false
 // "hitesh" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********* Operations *********
+
+let value = 3
+let negValue = -value
+// console.log((negValue));
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " Shubhra"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("2"+1+1);
+// console.log(2+1+"a");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// console.table([num1, num2, num3]);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);

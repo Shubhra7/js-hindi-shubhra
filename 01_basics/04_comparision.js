fa6dd8b41a7aa680c5c5096a9ebbdb6d593c42a0
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-03-30 16:11:07 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+// console.log("2" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// comparision operator first conver that null into 0 then compare that is why 3rd one is right and 1st one is not correct.
+
+// ===
+
+console.log("2" === 2);

a918285a2b41513c812209fa50f96da3489e6ae2
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-03-31 13:47:46 +0000

    data type complete

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -1,45 +1,70 @@
-// Primitive
+// // Primitive
 
-// 7 types: String, Numbers, Boolean, null, undefined, symbol, BigInt
+// // 7 types: String, Numbers, Boolean, null, undefined, symbol, BigInt
 
-const score = 100
-const scorevalue = 100.3
+// const score = 100
+// const scorevalue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-const bigNumber = 3555555554646n // will convert into bigInt
+// const bigNumber = 3555555554646n // will convert into bigInt
 
 
-// Reference (Non primitive)
+// // Reference (Non primitive)
 
-// Array, Objects, Functions
+// // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+// const heros = ["shaktiman", "naagraj", "doga"];
 
-let myObj =
-{
-    name: "hitesh",
-    age: 22,
+// let myObj =
+// {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myfucntions = function(){
+//     console.log("Hello shubhra");
+// }
+
+// console.log(typeof bignumber);
+
+// // Typeof
+// // undefined => undefined
+// // Null => Null
+// // Boolean => boolean
+// // String => String
+// // Object(native and does not implement) => object
+// // Object(native or host and does implement) => fucntion
+// // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive),  Heap (Non_Primitive)
+
+let myYoutubename = "ShubhrajitGhosh"
+
+let anothername = myYoutubename
+myYoutubename = "RockAll"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
-const myfucntions = function(){
-    console.log("Hello shubhra");
-}
+let userTwo = userOne
 
-console.log(typeof bignumber);
+userTwo.email = "Shubhra@future.in"
 
-// Typeof
-// undefined => undefined
-// Null => Null
-// Boolean => boolean
-// String => String
-// Object(native and does not implement) => object
-// Object(native or host and does implement) => fucntion
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+console.log(userOne.email);
+console.log(userTwo.email);

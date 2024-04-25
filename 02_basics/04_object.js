26cd01c0f4d7de51971d13f7d8076caaf970f690
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-04-25 18:26:09 +0000

    yup

---
+++ 02_basics/04_object.js
@@ -0,0 +1,55 @@
+// singleton object
+
+const tinderUser = {}
+
+tinderUser.id="123abc"
+tinderUser.name="Shubhra"
+tinderUser.isLoggedIn = false
+
+// console.table(tinderUser)
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Shubhrajit",
+            lastname:"Ghosh"
+        }
+    }
+}
+
+// console.log(regularUser.fullname)
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {6:"a", 5:"b"}
+
+
+// const obj3= Object.assign({},obj1,obj2,obj4)  //{} is target and rest are source
+// console.log(obj3)
+
+
+
+// spread operator for joining easily
+// const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3);
+
+
+user=[
+    {
+        name:"Shubhra",
+        email:"shu@gmail.com",
+        phone:94
+    },
+    {
+        name:"Debjit",
+        email:"deb@gmail.com",
+        phone:84
+    }
+]
+
+console.log(user[1].name)
+
+
+

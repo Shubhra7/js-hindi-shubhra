f7b4ab37673e178a4391bab6f4c0c074fbc71382
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-04-26 07:10:00 +0000

    done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -3,9 +3,41 @@
 if(true){
     let a=10
     const b=20
-    var c=30
+    // var c=30
 }
 
 // console.log(a)
 // console.log(b)
-console.log(c)   // so it is accessable out the scope so it will make us ambigious the values
+// console.log(c)   // so it is accessable out the scope so it will make us ambigious the values
+
+
+// the elder can't take ice cream from jr but the jr can take from elder
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+        const website ="youtube"
+       // console.log(username+website);
+    }
+}
+
+
+/*********interesting****** */
+
+
+// type: 1
+
+console.log(addOne(5))
+
+function addOne(num){
+    return num+1
+}
+
+
+
+//type: 2
+console.log(addTwo(4))   // in this type don't work beacuse the concept of hoisting
+
+const addTwo= function(num){
+    return num+2
+}
+

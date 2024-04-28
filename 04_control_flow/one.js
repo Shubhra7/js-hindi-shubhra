bebc9f972056b962da74153af7842cb5507aaeba
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-04-28 04:57:13 +0000

    condition done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// if
+
+const isUserLoggedIn = true
+
+if(isUserLoggedIn){
+    console.log("hi")
+}
+
+const balance=1000
+
+if(balance>500) console.log("test");
+
+
+const userLoggedIn = true
+const debitcard = true
+
+if( userLoggedIn && debitcard){
+    console.log("Allow to buy course")
+}
+
+const loggedInFromGoogle = true
+const loggedInEmail =true
+
+if(loggedInEmail || loggedInFromGoogle){
+    console.log("user Logged in");
+}

51f9c62d5fdec0b0b8dc521b3c5b1a77baa25307
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-04-28 05:20:05 +0000

    loop done

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+// let index=0
+// while(index <= 10){
+//     console.log(`value of index is ${index}`)
+//     index+=2
+// }
+
+
+// let array = ['flash','superman', 'Batman']
+// let arr=0
+// while(arr < array.length){
+//     console.log(`Value is ${array[arr]}`)
+//     arr+=1
+// }
+
+
+// let score = 11
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while (score<10);
+

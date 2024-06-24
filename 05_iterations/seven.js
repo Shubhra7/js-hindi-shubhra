107b1010e56bb04d79244724cfae080a0336881e
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-06-24 14:55:40 +0000

    forEach loop main value change in seven.js

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -9,11 +9,24 @@ const newNums = [1,2,3,4,5,6,7,8,9,10]
 
 
 // chaining 
-const ans1= newNums
-    .map((item)=> item *10)
-    .map((item)=> item+1)
-    .filter((item) => item>35)
+// const ans1= newNums
+//     .map((item)=> item *10)
+//     .map((item)=> item+1)
+//     .filter((item) => item>35)
 
-console.log(ans1);
+// console.log(ans1);
+
+
+
+//**** How can you change value of the main through forEach loop */
+// good concept to change make it in foreach also 
+const numbers = [65, 44, 12, 4];
+numbers.forEach(myFunction)
+
+function myFunction(item, index, arr) {
+  arr[index] = item * 10;
+}
+
+console.log(numbers);
 
 
